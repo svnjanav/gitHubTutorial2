8271d73c3f6563f5df23de7cd840630f7c537ab4
svnjanav <sanjanavkanth@gmail.com>
2024-07-12 12:00:29 -0700

    Initial Commit

---
+++ gitHubTutorial2/gitHubTutorial2App.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubTutorial2App.swift
+//  gitHubTutorial2
+//
+//  Created by Sanjana V on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct gitHubTutorial2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
